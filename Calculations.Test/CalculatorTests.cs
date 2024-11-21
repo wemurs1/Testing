117393e592b34c50ddb1bb9b510bd5ad55a8d46b
Stuart Murray <stuart.murray.dev@outlook.com>
2024-11-21 15:35:16 +0000

    baseline

---
+++ Calculations.Test/CalculatorTests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Calculations.Test;
+
+public class CalculatorTests
+{
+    [Fact]
+    public void TestAdd()
+    {
+        Assert.True(true);
+    }
+}
---
+++ Calculations/Calculator.cs
@@ -0,0 +1,9 @@
+namespace Calculations;
+
+public class Calculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
