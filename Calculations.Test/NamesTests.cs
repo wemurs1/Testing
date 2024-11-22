6b8dec1536d596e1e58029c5595cdd4f87a45fd1
Stuart Murray <stuart.murray.dev@outlook.com>
2024-11-22 10:20:48 +0000

    check property is null

--- Calculations.Test/NamesTests.cs
+++ Calculations.Test/NamesTests.cs
@@ -13,4 +13,11 @@ public class NamesTests
         // Assert.EndsWith("Brown", result);
         Assert.Matches("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+", result);
     }
+
+    [Fact]
+    public void NickName_MustBeNull()
+    {
+        var names = new Names();
+        Assert.Null(names.NickName);
+    }
 }
--- Calculations/Names.cs
+++ Calculations/Names.cs
@@ -2,6 +2,8 @@ namespace Calculations;
 
 public class Names
 {
+    public string? NickName { get; set; }
+
     public string MakeFullName(string firstName, string lastName)
     {
         return $"{firstName} {lastName}";
